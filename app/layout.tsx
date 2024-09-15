1e8e8743db83d83ef08a2eb1d561a8bc1307eb21
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-09-15 04:51:58 -0400

    Add @vercel/analytics to project
    Included @vercel/analytics package to the project dependencies in package.json and updated pnpm-lock.yaml accordingly. Integrated the Analytics component from @vercel/analytics/react in layout.tsx to enable usage analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import React from "react";
+import {Analytics} from "@vercel/analytics/react"
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "JEA BizDev",
-  description: "Strategic advisory services for groundbreaking partnerships across North America, Europe, and Asia.",
+	title: "JEA BizDev",
+	description: "Strategic advisory services for groundbreaking partnerships across North America, Europe, and Asia.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+									   children,
+								   }: Readonly<{
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+		<body
+			className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+		>
+		{children}
+		</body>
+		<Analytics/>
+		</html>
+	);
 }
